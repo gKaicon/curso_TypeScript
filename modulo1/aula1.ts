70c89fdaf041d08fb48b2eee7c80c02fe34272e9
Gabriel Káicon <gkaicon2@gmail.com>
2024-03-12 21:42:10 -0300

    Inicio do Curso da EspecializaTI

---
+++ modulo1/aula1.ts
@@ -0,0 +1,7 @@
+function multiply(a: number, b: number) :number {
+    return a * b;
+}
+console.log(multiply(4, 5));//20
+//multiply("4", 5);//error
+
+export {multiply}
---
+++ modulo2/tipos/types.ts
@@ -0,0 +1,3 @@
+let name_var1: number
+let name_var2: string
+let name_var3: boolean
